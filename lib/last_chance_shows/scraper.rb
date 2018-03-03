10f2cf8aacdf763eee5ba036acfdc9b487372437
Chris Henry <cdhenry@gmail.com>
2018-03-03 11:58:06 -0500

    Update scraper.rb

--- lib/last_chance_shows/scraper.rb
+++ lib/last_chance_shows/scraper.rb
@@ -1,31 +1,31 @@
 class LastChanceShows::Scraper
 
-  def self.get_page
+  def get_page
     Nokogiri::HTML(open("http://www.playbill.com/article/last-chance-schedule-of-upcoming-broadway-and-off-broadway-show-closings"))
   end
 
-  def self.get_shows
-    self.get_page.css(".bsp-article-content").children #all the elements in the .bsp-article-content class
+  def get_shows
+    get_page.css(".bsp-article-content").children #all the elements in the .bsp-article-content class
   end
 
-  def self.make_shows
+  def make_shows
     closing_i = 0   #index counter for keeping track of the 'Closing..' <h2>'s
-    self.get_shows.each_with_index do |element, i| #find shows and closings by iterating through each child of '.bsp-article-content'
+    get_shows.each_with_index do |element, i| #find shows and closings by iterating through each child of '.bsp-article-content'
       if element.name == "h2" #capture and index for each 'Show Closing' date
         closing_i = i
       end
 
       if element.name == "p" && closing_i > 0 && !(element.text.include?("To purchase")) && element.children[0].name == "u" #find the <p>'s that contain show information based on current site format (2/26/2018)
         title = element.children[0].text #assign title based on text in first child of <p>
-        closing = self.get_shows[closing_i].text #assign closing based on text in the last <h2> field
-        url = self.make_show_url(title) #assign url based on title
+        closing = get_shows[closing_i].text #assign closing based on text in the last <h2> field
+        url = make_show_url(title) #assign url based on title
 
-        LastChanceShows::Show.new(title, closing, url) unless self.closing_to_date(closing) < Date.today
+        LastChanceShows::Show.new(title, closing, url) unless closing_to_date(closing) < Date.today
       end
     end
   end
 
-  def self.make_search_url(title)
+  def make_search_url(title)
     search_url = "http://www.playbill.com/searchpage/search?q="
     title_parse = title.split(" ")
     title_parse.each do |word|
@@ -37,7 +37,7 @@ class LastChanceShows::Scraper
     search_url = search_url + "&sort=Relevance&shows=on&qasset="
   end
 
-  def self.make_show_url(title)
+  def make_show_url(title)
     linkpage = Nokogiri::HTML(open(make_search_url(title)))
     show_links = linkpage.css(".bsp-list-promo-title a")
 
@@ -57,7 +57,7 @@ class LastChanceShows::Scraper
     end
   end
 
-  def self.closing_to_date(closing)
+  def closing_to_date(closing)
     day = Date::DAYNAMES.detect{|d| closing.include?(d)}
     month = Date::MONTHNAMES.detect{|m| closing.include?(m) unless m == nil}
     month_integer = Date::MONTHNAMES.index(month)
