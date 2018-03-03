36c7b4ec79efb3ffa04d4dda9b225754893a89d2
Chris Henry <cdhenry@gmail.com>
2018-03-03 09:23:54 -0500

    Scraper and Show Refactor

--- lib/last_chance_shows.rb
+++ lib/last_chance_shows.rb
@@ -4,8 +4,8 @@ require 'pry'
 
 require_relative "./last_chance_shows/version"
 require_relative "./last_chance_shows/cli"
-require_relative "./last_chance_shows/shows"
-require_relative "./last_chance_shows/show-info"
+require_relative "./last_chance_shows/show"
+require_relative "./last_chance_shows/scraper"
 
 require "last_chance_shows/version"
 
